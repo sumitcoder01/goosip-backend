f79eac1e2245e708bea2c2fadb71c5a228bd036e
sumitcoder01 <135352466+sumitcoder01@users.noreply.github.com>
2024-02-07 19:04:19 +0530

    socket added

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express');
+const { Server } = require("socket.io")
+const { createServer } = require("http");
+
+const app = express();
+const server = createServer(app);
+const port = process.env.PORT || 3000;
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("User Joined", socket.id);
+
+    socket.on("send-message", ({ room, message }) => {
+        console.log({ room, message });
+        io.to(room).emit("receive-message", message);
+    });
+
+    socket.on("join-room", room => {
+        socket.join(room);
+        console.log(`User joined room ${room}`);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User leaved", socket.id);
+    });
+
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
